903b802a9a91368a88aaf0ce8f8884c368d1c4c9
isnaya-andrade <isnayaandrade@gmail.com>
2024-11-27 14:02:53 -0600

    Change Use

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const PORT = 15000
 
 const sitesRouter = require('./routers/sitesRouter.js')
 
-app.use("/sites", sitesRouter)
 app.use(express.json({ limit: '50mb'}))
 app.use(express.urlencoded({ limit: '50mb', extended: true}))
+app.use("/sites", sitesRouter)
 
 app.listen(PORT, () => {
     console.log(`Server iniciado en http://localhost:${PORT}`);
